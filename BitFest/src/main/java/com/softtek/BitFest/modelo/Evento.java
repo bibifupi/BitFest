29e617ce1ce7eb02149fe809ab43c742a5bffd46
MauroOrtizGamonal <mauroog22112004@gmail.com>
2024-05-07 16:25:17 +0200

    modificacion

--- BitFest/src/main/java/com/softtek/BitFest/modelo/Evento.java
+++ BitFest/src/main/java/com/softtek/BitFest/modelo/Evento.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @AllArgsConstructor
 @Entity
 @Table(name = "eventos")
+@Table(name = "eventos2")
 public class Evento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
