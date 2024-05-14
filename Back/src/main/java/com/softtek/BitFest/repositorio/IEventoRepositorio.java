803aeb905e54888c7f21865c66c0e474b3c99a85
debora.fuentes <bibifuentespiris@gmail.com>
2024-05-14 15:58:23 +0200

    evento y evento categoria querys

--- Back/src/main/java/com/softtek/BitFest/repositorio/IEventoRepositorio.java
+++ Back/src/main/java/com/softtek/BitFest/repositorio/IEventoRepositorio.java
@@ -2,6 +2,17 @@ package com.softtek.BitFest.repositorio;
 
 import com.softtek.BitFest.modelo.Comentario;
 import com.softtek.BitFest.modelo.Evento;
+import com.softtek.BitFest.modelo.Organizador;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IEventoRepositorio  extends IGenericoRepositorio<Evento,Integer>{
+    List<Evento>findFirst7ByFechaInicio(String fechaInicio);
+
+    List<Evento>findByTituloLike(String titulo);
+
+    @Query("select e.organizador from Evento e where e.organizador.nombre = :nombre")
+    List<Organizador> consultaEventoPorOrganizador(@Param("nombre") String nombre);
 }
--- Back/src/main/java/com/softtek/BitFest/servicio/EventoImpl.java
+++ Back/src/main/java/com/softtek/BitFest/servicio/EventoImpl.java
@@ -2,11 +2,14 @@ package com.softtek.BitFest.servicio;
 
 import com.softtek.BitFest.modelo.Comentario;
 import com.softtek.BitFest.modelo.Evento;
+import com.softtek.BitFest.modelo.Organizador;
 import com.softtek.BitFest.repositorio.IComentarioRepositorio;
 import com.softtek.BitFest.repositorio.IEventoRepositorio;
 import com.softtek.BitFest.repositorio.IGenericoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EventoImpl extends CRUDImpl<Evento, Integer> implements IEventoServicio {
@@ -15,5 +18,20 @@ public class EventoImpl extends CRUDImpl<Evento, Integer> implements IEventoServ
 
     @Override
     protected IGenericoRepositorio<Evento, Integer> getRepo() { return eventRepo;
+    }
+
+    @Override
+    public List<Evento> findFirst7ByFechaInicio(String fechaInicio) {
+        return eventRepo.findFirst7ByFechaInicio(fechaInicio);
+    }
+
+    @Override
+    public List<Evento> findByTituloLike(String titulo) {
+        return eventRepo.findByTituloLike(titulo);
+    }
+
+    @Override
+    public List<Organizador> consultaEventoPorOrganizador(String nombre) {
+        return eventRepo.consultaEventoPorOrganizador(nombre);
     }
 }
