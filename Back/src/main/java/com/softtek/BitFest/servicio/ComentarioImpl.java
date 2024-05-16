2d8f9bce7c611f7f4e075523508688a2a9c1c865
Hhervellagonzalez <hhervellagonzalez@gmail.com>
2024-05-16 13:04:27 +0200

    Actualizados IComentarioRepositorio, IcomentarioServicio y ComentarioImpl

--- Back/src/main/java/com/softtek/BitFest/servicio/ComentarioImpl.java
+++ Back/src/main/java/com/softtek/BitFest/servicio/ComentarioImpl.java
@@ -3,15 +3,26 @@ package com.softtek.BitFest.servicio;
 import com.softtek.BitFest.modelo.Comentario;
 import com.softtek.BitFest.repositorio.IComentarioRepositorio;
 import com.softtek.BitFest.repositorio.IGenericoRepositorio;
+import com.softtek.BitFest.servicio.CRUDImpl;
+import com.softtek.BitFest.servicio.IComentarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ComentarioImpl extends CRUDImpl<Comentario,Integer> implements IComentarioServicio{
+public class ComentarioImpl extends CRUDImpl<Comentario, Integer> implements IComentarioServicio {
+
     @Autowired
     private IComentarioRepositorio comentRepo;
 
     @Override
     protected IGenericoRepositorio<Comentario, Integer> getRepo() {
-        return comentRepo;}
+        return comentRepo;
+    }
+
+    @Override
+    public List<Comentario> findByEventoId(int eventoId) {
+        return comentRepo.findByEventoId(eventoId);
+    }
 }
