842aca64a13f84a66cfe606a75cc524877d05c25
debora.fuentes <bibifuentespiris@gmail.com>
2024-05-16 14:19:06 +0200

    DTO

---
+++ Back/src/main/java/com/softtek/BitFest/dto/EventoCategoriaDTO.java
@@ -0,0 +1,33 @@
+package com.softtek.BitFest.dto;
+
+import com.softtek.BitFest.modelo.Categoria;
+import com.softtek.BitFest.modelo.Evento;
+import com.softtek.BitFest.modelo.EventoCategoria;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class EventoCategoriaDTO {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "eventoId")
+    private Evento evento;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "categoriaId")
+    private Categoria categoria;
+
+    public EventoCategoria castEventoCategoria() {
+        EventoCategoria ec1 = new EventoCategoria();
+        ec1.setEvento(this.evento);
+        ec1.setCategoria(this.categoria);
+        return ec1;
+    }
+
+    public EventoCategoriaDTO castEventoCategoriaDTO(EventoCategoria ec) {
+        evento = ec.getEvento();
+        categoria = ec.getCategoria();
+        return this;
+    }
+}
