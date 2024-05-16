d2d2b633167944c7639e90b50875c38ce967a90e
CampusDaniMelero <daniel.melerovilches.23@campusfp.es>
2024-05-16 13:40:18 +0200

    Añadido Servicio de Usuario

--- Back/src/main/java/com/softtek/BitFest/servicio/IUsuarioServicio.java
+++ Back/src/main/java/com/softtek/BitFest/servicio/IUsuarioServicio.java
@@ -4,4 +4,5 @@ import com.softtek.BitFest.modelo.Categoria;
 import com.softtek.BitFest.modelo.Usuario;
 
 public interface IUsuarioServicio extends ICRUD<Usuario,Integer> {
+    <List>Usuario findByNombreAsc(String nombre);
 }
