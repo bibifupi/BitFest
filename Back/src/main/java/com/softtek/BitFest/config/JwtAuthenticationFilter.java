55c8b3aa898daece89c9c3ddef499410bbd6e669
CampusDaniMelero <daniel.melerovilches.23@campusfp.es>
2024-05-21 10:25:32 +0200

    Realizado DTORequest y corregido temporalmente el UsuarioImpl , faltan los controladores y securityConfig

---
+++ Back/src/main/java/com/softtek/BitFest/config/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.softtek.BitFest.config;
+
+import com.softtek.BitFest.servicio.IUsuarioServicio;
+import com.softtek.BitFest.servicio.JwtService;
+import org.springframework.lang.NonNull;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    /*
+    * Recupere el correo electrónico del usuario analizando el token de portador y posteriormente busque
+    * la información del usuario correspondiente en la base de datos.
+    Verifica la autenticidad del JWT.
+    Genere un objeto de autenticación utilizando el nombre de usuario y la contraseña proporcionados y,
+    * posteriormente, guárdelo en el archivo SecurityContextHolder.
+    *
+    *
+    * */
+    private final JwtService jwtService;
+    private final IUsuarioServicio userService;
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
+        if (StringUtils.isNotEmpty(userEmail)
+                && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.userDetailsService()
+                    .loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                context.setAuthentication(authToken);
+                SecurityContextHolder.setContext(context);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- Back/src/main/java/com/softtek/BitFest/servicio/UsuarioImpl.java
+++ Back/src/main/java/com/softtek/BitFest/servicio/UsuarioImpl.java
@@ -4,6 +4,9 @@ import com.softtek.BitFest.modelo.Usuario;
 import com.softtek.BitFest.repositorio.IGenericoRepositorio;
 import com.softtek.BitFest.repositorio.IUsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,4 +18,24 @@ public class UsuarioImpl extends CRUDImpl<Usuario, Integer> implements IUsuarioS
     protected IGenericoRepositorio<Usuario, Integer> getRepo() {
         return usuRepo;
     }
+
+    @Override
+    public Usuario findByNombreAsc(String nombre) {
+        return (Usuario) usuRepo.findByNombreAsc(nombre);
+    }
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username)  {
+                Usuario user = (Usuario) usuRepo.findByNombreAsc(username);
+                if (user == null) {
+                    throw new UsernameNotFoundException("Usuario no encontrado");
+                }
+                return (UserDetails) user;
+            }
+        };
+    }
+
 }
