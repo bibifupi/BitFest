6c5817fd62b93f4febaa96849276d03d5a239e14
MauroOrtizGamonal <mauroog22112004@gmail.com>
2024-05-21 15:09:14 +0200

    IcategoriaServicio,CategoriaImpl,IlikeServicio y LikeImpl modificados funcionando

--- Back/src/main/java/com/softtek/BitFest/servicio/ILikeServicio.java
+++ Back/src/main/java/com/softtek/BitFest/servicio/ILikeServicio.java
@@ -1,7 +1,11 @@
 package com.softtek.BitFest.servicio;
 
-import com.softtek.BitFest.modelo.Categoria;
 import com.softtek.BitFest.modelo.Like;
 
+import java.util.List;
+
 public interface ILikeServicio extends ICRUD<Like,Integer> {
+    List<Like> findByUsuarioId(Integer usuarioId);
+    long countByUsuarioId(Integer usuarioId);
+    boolean existsByUsuarioIdAndPostId(Integer usuarioId, Integer postId);
 }
--- Back/src/main/java/com/softtek/BitFest/servicio/LikeImpl.java
+++ Back/src/main/java/com/softtek/BitFest/servicio/LikeImpl.java
@@ -6,13 +6,31 @@ import com.softtek.BitFest.repositorio.ILikeRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LikeImpl extends CRUDImpl<Like, Integer> implements ILikeServicio {
     @Autowired
-    private ILikeRepositorio catRepo;
+    private ILikeRepositorio likeRepo;
 
     @Override
     protected IGenericoRepositorio<Like, Integer> getRepo() {
-        return catRepo;
+        return likeRepo;
+    }
+
+    // Implementación de los métodos de ILikeRepositorio
+    @Override
+    public List<Like> findByUsuarioId(Integer usuarioId) {
+        return likeRepo.findByUsuarioId(usuarioId);
+    }
+
+    @Override
+    public long countByUsuarioId(Integer usuarioId) {
+        return likeRepo.countByUsuarioId(usuarioId);
+    }
+
+    @Override
+    public boolean existsByUsuarioIdAndPostId(Integer usuarioId, Integer postId) {
+        return likeRepo.existsByUsuarioIdAndPostId(usuarioId, postId);
     }
 }
