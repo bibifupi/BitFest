d4c92683ad7e170ef98ac198bfa1699e10ca3557
debora.fuentes <bibifuentespiris@gmail.com>
2024-05-22 09:51:04 +0200

    reempezando

--- Back/src/main/java/com/softtek/BitFestApplication.java
+++ Back/src/main/java/com/softtek/BitFestApplication.java
@@ -1,4 +1,4 @@
-package com.softtek.BitFest;
+package com.softtek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Back/src/test/java/com/softtek/BitFestApplicationTests.java
+++ Back/src/test/java/com/softtek/BitFestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.softtek.BitFest;
+package com.softtek;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
