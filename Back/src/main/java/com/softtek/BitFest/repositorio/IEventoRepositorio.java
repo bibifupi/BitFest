4a7a9d5b53dcb05060a3071ddbf892b3c11a547d
Hhervellagonzalez <hhervellagonzalez@gmail.com>
2024-05-22 13:19:48 +0200

    Cambios en IEvento

--- Back/src/main/java/com/softtek/BitFest/repositorio/IEventoRepositorio.java
+++ Back/src/main/java/com/softtek/BitFest/repositorio/IEventoRepositorio.java
@@ -2,5 +2,10 @@ package com.softtek.BitFest.repositorio;
 
 import com.softtek.BitFest.modelo.Evento;
 
+import java.util.List;
+
 public interface IEventoRepositorio extends IGenericoRepositorio<Evento,Integer>{
+    List<Evento> findFirst7ByFechaInicio(String fechaInicio);
+    List<Evento> findByTituloLike(String titulo);
+    List<Evento> findAllByOrderByFechaInicio();
 }
