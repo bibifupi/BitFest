cf364c37b9bcba9b1eb0300067e221f799a9b007
Hhervellagonzalez <hhervellagonzalez@gmail.com>
2024-05-22 10:03:19 +0200

    Eliminacion

--- Back/src/main/java/com/softtek/BitFest/repositorio/IGenericoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.softtek.BitFest.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface IGenericoRepositorio<T,ID> extends JpaRepository<T, ID> {
-}
